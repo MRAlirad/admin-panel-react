798a01dd3660807ca1d7c32707ce316733f56470
MRAlirad <m.r.alirad75@gmail.com>
2023-09-21 00:39:49 +0330

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Sidebar from './components/Sidebar';
+import Header from './components/Header';
 
 function App() {
 	return (
 		<div className="admin-panel-app bg-ghostWhite min-h-screen grid grid-cols-[15%_1fr] grid-rows-[max-content_1fr]">
 			<Sidebar />
+			<Header />
 		</div>
 	);
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,43 @@
+import profileImg from '../assets/pics/Profile.png'
+
+const Header = () => {
+	const title = 'پروفایل';
+	return (
+		<div className="flex items-center justify-between w-full h-[160px] pr-4 pl-12">
+			<div className="title-navigation-box">
+				<span className="navigation text-sm text-powderBlue"> صفحات / {title}</span>
+				<h1 className="page-title text-[34px] text-delftBlue font-bold">{title}</h1>
+			</div>
+			<div className="settings-search-box grid grid-cols-[1fr_max-content_max-content] items-center gap-2 w-[420px] h-16 p-2.5 bg-white rounded-[30px]">
+				<div className="search-box flex items-center gap-1 h-full py-1 px-3 rounded-[30px] bg-ghostWhite">
+					<button className="btn icon text-delftBlue">
+						<i className="material-icons material-icons-outlined"> search </i>
+					</button>
+					<input
+						type="text"
+						className="text-sm text-delftBlue"
+					/>
+				</div>
+				<div className="actions-box flex items-center gap-2">
+					<button className="btn icon text-powderBlue">
+						<i className="material-icons material-icons-outlined"> notifications </i>
+					</button>
+					<button className="btn icon text-powderBlue">
+						<i className="material-icons material-icons-round"> nightlight_round </i>
+					</button>
+					<button className="btn icon text-powderBlue">
+						<i className="material-icons material-icons-outlined"> info </i>
+					</button>
+				</div>
+				<div className="img-box w-10 h-10 rounded-full">
+					<img
+						src={profileImg}
+						alt="profile"
+					/>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Header;
