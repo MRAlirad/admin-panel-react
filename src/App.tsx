e4426624367a38d0c29437f4657c982b835b71c0
MRAlirad <m.r.alirad75@gmail.com>
2023-09-20 23:35:15 +0330

    commits

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
-  return (
-    <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
-  )
+	return (
+		<>
+			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+		</>
+	);
 }
 
-export default App
+export default App;
