88541b04fae324d0a3d852f10df341d2a2d31c56
MRAlirad <m.r.alirad75@gmail.com>
2023-09-21 00:09:51 +0330

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                ghostWhite: '#F4F7FE',
+                blue: '#4318FF',
+                delftBlue: '#2B3674',
+                powderBlue: '#A3AED0',
+            }
+        },
+    },
+    plugins: [],
+};
