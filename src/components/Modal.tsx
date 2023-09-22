71f12978486dd893b736f5a17ab8730c8325d86c
MRAlirad <m.r.alirad75@gmail.com>
2023-09-23 00:04:31 +0330

    modal

---
+++ src/components/Modal.tsx
@@ -0,0 +1,28 @@
+import { ReactNode } from 'react';
+
+interface Props {
+	title: string;
+	onClose: () => void;
+	children: ReactNode;
+}
+
+const Modal = ({ title, onClose, children }: Props) => {
+	return (
+		<div className="backdrop fixed w-full h-full top-0 right-0 bg-[#000b] flex  items-center justify-center">
+			<div className="modal-segment card flex flex-col items-center justify-center gap-5 p-5">
+				<div className="title-close-box flex items-center justify-between w-full">
+					<h3 className="title text-xl text-delftBlue font-bold">{title}</h3>
+					<button
+						onClick={onClose}
+						className="btn icon text-red"
+					>
+						<i className="material-icons material-icons-round">close</i>
+					</button>
+				</div>
+				<div className="modal-container">{children}</div>
+			</div>
+		</div>
+	);
+};
+
+export default Modal;
