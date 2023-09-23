ad9a2079235aabd25e765e4a9f727df0189f0a51
MRAlirad <m.r.alirad75@gmail.com>
2023-09-23 16:00:19 +0330

    modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -9,15 +9,23 @@ interface Props {
 
 const Modal = ({ title, onClose, children }: Props) => {
 	return (
-		<div className="backdrop fixed w-full h-full top-0 right-0 bg-[#000b] flex  items-center justify-center">
-			<div className="modal-segment card flex flex-col items-center justify-center gap-5 p-5">
-				<div className="title-close-box flex items-center justify-between w-full">
-					<h3 className="title text-xl text-delftBlue font-bold">{title}</h3>
-					<Button type='icon' text='close' onClick={onClose} color='red' />
+		<>
+			<div className="backdrop fixed top-0 right-0 w-full h-full bg-[#000b]"></div>
+			<div className="modal-segment fixed top-0 right-0 w-full h-full flex items-center justify-center">
+				<div className="modal-segment card grid grid-rows-[max-content_1fr] gap-5 w-max max-w-[95vw] h-max max-h-[95vh] p-5">
+					<div className="title-close-box flex items-center justify-between w-full">
+						<h3 className="title text-xl text-delftBlue font-bold">{title}</h3>
+						<Button
+							type="icon"
+							text="close"
+							onClick={onClose}
+							color="red"
+						/>
+					</div>
+					<div className="modal-container overflow-y-auto overflow-x-hidden">{children}</div>
 				</div>
-				<div className="modal-container">{children}</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
