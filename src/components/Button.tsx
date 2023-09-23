088e3193194a17cf64481a1a64071f7daada41ce
MRAlirad <m.r.alirad75@gmail.com>
2023-09-23 20:41:21 +0330

    button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,21 +4,28 @@ interface Props {
 	text: string;
 	onClick: () => void;
 	fluid?: boolean;
+	disabled?: boolean;
 }
 
-const Button = ({ type, color, text, onClick, fluid = false }: Props) => {
-	return(
+const Button = ({ type, color, text, onClick, fluid = false, disabled = false}: Props) => {
+	return (
 		<button
-			className={`flex items-center justify-center ${fluid ? 'w-full' : 'w-max'} ${
-				type === 'icon'
-					? `border-none outline-none p-1 text-${color}`
-					: type === 'primary'
-					? `bg-${color} text-white py-2.5 px-10 border border-solid border-${color} rounded-lg`
-					: type === 'secondary'
-					? `bg-white text-${color} py-2.5 px-10 border border-solid border-${color} rounded-lg`
-					: ''
-			} opacity-95 hover:opacity-100`}
+			className={`
+				flex items-center justify-center
+				${fluid ? 'w-full' : 'w-max'}
+				${
+					type === 'icon'
+						? `border-none outline-none p-1 text-${color} text-red`
+						: type === 'primary'
+						? `bg-${color} bg-delftBlue text-white py-2.5 px-10 border border-solid border-${color} rounded-lg`
+						: type === 'secondary'
+						? `bg-white text-${color} py-2.5 px-10 border border-solid border-${color} rounded-lg`
+						: ''
+				}
+				${disabled ? 'opacity-60' : 'opacity-95 hover:opacity-100'}
+			`}
 			onClick={onClick}
+			disabled={disabled}
 		>
 			{type === 'icon' ? <i className="material-icons material-icons-round">{text}</i> : text}
 		</button>
