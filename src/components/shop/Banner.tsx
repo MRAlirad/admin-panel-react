034b182725dfcea9b451878a0413c0d267af2b4b
MRAlirad <m.r.alirad75@gmail.com>
2023-09-25 22:58:15 +0330

    banner

--- src/components/shop/Banner.tsx
+++ src/components/shop/Banner.tsx
@@ -1,21 +1,21 @@
-import Button from "../Button";
+import Button from '../Button';
 
 const Banner = () => {
-	return(
-        <div className="banner bg-[url('/src/assets/pics/ShopBanner.png')] bg-cover bg-no-repeat bg-left h-56 w-full rounded-4" >
-            <div className="desc w-1/2 h-full grid gap-4 ">
-                <h3 className="title">
-
-                </h3>
-                <p className="desc">
-
-                </p>
-                <div className="action-box">
-                    <Button type="white" text="ببین و بگرد" onClick={()=> {}} />
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className="banner bg-[url('/src/assets/pics/ShopBanner.png')] bg-cover bg-no-repeat bg-left h-56 w-full rounded-xl">
+			<div className="desc w-1/2 h-full flex flex-col justify-center gap-4 pr-[5%] text-white">
+				<h3 className="title text-xl">لورم ایپسوم متن ساختگی</h3>
+				<p className="desc text-sm">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از</p>
+				<div className="action-box">
+					<Button
+						type="white"
+						text="ببین و بگرد"
+						onClick={() => {}}
+					/>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Banner;
