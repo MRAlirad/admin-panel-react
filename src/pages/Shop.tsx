7d6f254e4289e77d971ae92c8c025678784ed4a7
MRAlirad <m.r.alirad75@gmail.com>
2023-09-26 00:18:02 +0330

    shop

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,9 +1,12 @@
 import Banner from '../components/shop/Banner';
+import TodayThrends from '../components/shop/TodayThrends';
 
 const Shop = () => {
 	return (
 		<div className="grid grid-cols-[2fr_1fr]">
 			<Banner />
+			<div></div>
+			<TodayThrends />
 		</div>
 	);
 };
