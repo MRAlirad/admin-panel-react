1c434b23621b592a1f52175aad9e74afd88a2036
MRAlirad <m.r.alirad75@gmail.com>
2023-09-26 16:35:40 +0330

    thrend item

--- src/components/shop/ThrendItem.tsx
+++ src/components/shop/ThrendItem.tsx
@@ -8,33 +8,39 @@ interface Props {
 	price: number;
 	link: string;
 	liked: boolean;
+	group: string;
 }
 
-const ThrendItem = ({ imageSrc, title, description, price, link, liked}: Props) => {
+const ThrendItem = ({ imageSrc, title, description, price, link, liked, group }: Props) => {
 	const [like, setLike] = useState(liked);
 
 	return (
-		<div className="thrend-item card p-5">
-			<div className="img-box relative">
+		<div className="thrend-item card p-3">
+			<div className="img-box relative rounded-md">
 				<img
 					src={imageSrc}
 					alt={title}
 				/>
 				<button
-					className="like-btn flex items-center justify-center w-7 h-7 text-base bg-white absolute top-3 left-3"
+					className="like-btn flex items-center justify-center w-6 h-6 bg-white absolute top-2 left-2 rounded-full text-base text-red"
 					onClick={() => setLike(!like)}
 				>
-					{like}
+					<i className="material-icons material-icons-round"> {like ? 'favorite' : 'favorite_border'} </i>
 				</button>
+				<span className="flex items-center justify-center w-5 h-5 bg-white absolute top-2 right-2 rounded-full text-sm text-delftBlue">
+					<i className="material-icons material-icons-round"> {group} </i>
+				</span>
 			</div>
-			<div className="specs-box">
-				<h4 className="title">{title}</h4>
-				<p className="desc">{description}</p>
-				<div className="price-purchace-box">
-					<div className="price">قیمت: {price} تومان</div>
+			<div className="specs-box grid gap-4 pt-4 pb-2">
+				<div className="title-desc-box grid gap-1">
+					<h4 className="title text-delftBlue text-base font-bold line-clamp-1">{title}</h4>
+					<p className="desc text-xs text-powderBlue line-clamp-1">{description}</p>
+				</div>
+				<div className="price-purchace-box flex items-center justify-between">
+					<div className="price text-xs text-neonBlue">قیمت: {price} تومان</div>
 					<Link
 						to={link}
-						className="flex items-center justify-center text-sm text-white bg-delftBlue w-20 h-7"
+						className="flex items-center justify-center text-sm text-white bg-delftBlue w-20 h-7 rounded-full"
 					>
 						خرید
 					</Link>
