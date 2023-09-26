a481b41b9e3530acd9979d4e0a3f49007559e4c1
MRAlirad <m.r.alirad75@gmail.com>
2023-09-26 16:35:54 +0330

    shop

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -3,7 +3,7 @@ import TodayThrends from '../components/shop/TodayThrends';
 
 const Shop = () => {
 	return (
-		<div className="grid grid-cols-[2fr_1fr]">
+		<div className="grid grid-cols-[2fr_1fr] gap-y-10">
 			<Banner />
 			<div></div>
 			<TodayThrends />
