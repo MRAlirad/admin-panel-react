4003ab6f8fecff8cf0602dd7a0ea06b19376f8bd
MRAlirad <m.r.alirad75@gmail.com>
2023-09-28 13:42:46 +0330

    procuct containerr get from server

--- src/entities/ProductCategory.ts
+++ src/entities/ProductCategory.ts
@@ -1,23 +1,23 @@
 const ProductCategory = [
     {
+        type: 'all',
+        name : 'همه',
+    },
+    {
         type: 'music',
         name: 'موزیک',
-        icon: 'music_note',
     },
     {
         type: 'family',
         name: 'خانواده',
-        icon: 'diversity_1',
     },
     {
         type: 'art',
         name: 'هنر',
-        icon: 'palette',
     },
     {
         type: 'sport',
         name: 'ورزش',
-        icon: 'emoji_events',
     },
 ];
 
