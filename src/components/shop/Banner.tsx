60ec4ed254a6d00254c3a16ff07b35dee74cc636
MRAlirad <m.r.alirad75@gmail.com>
2023-09-29 23:58:38 +0330

    banner

--- src/components/shop/Banner.tsx
+++ src/components/shop/Banner.tsx
@@ -5,10 +5,13 @@ const Banner = () => {
 		<div className="banner bg-[url('/src/assets/pics/ShopBanner.png')] bg-cover bg-no-repeat bg-left h-56 w-full rounded-xl">
 			<div className="desc w-1/2 h-full flex flex-col justify-center gap-4 pr-[5%] text-white">
 				<h3 className="title text-xl">لورم ایپسوم متن ساختگی</h3>
-				<p className="desc text-sm">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از</p>
+				<p className="desc text-sm">
+					لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از
+				</p>
 				<div className="action-box">
 					<Button
-						type="white"
+						type="primary"
+						color="white"
 						text="ببین و بگرد"
 						onClick={() => {}}
 					/>
