d6b0b97a13b3a804aad7dcb8178b974a85174a13
MRAlirad <m.r.alirad75@gmail.com>
2023-09-29 22:29:57 +0330

    loader

---
+++ src/components/Loader.tsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+	return (
+		<div className="flex items-center justify-center w-full h-full">
+			<span className="flex w-16 h-16 border-4 border-solid border-transparent border-t-delftBlue rounded-full animate-spin"></span>
+		</div>
+	);
+};
+
+export default Loader;
