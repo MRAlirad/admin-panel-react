e37904d35d403c3dbb98c0d823d8e2cc1bab68e6
MRAlirad <m.r.alirad75@gmail.com>
2023-10-07 21:01:38 +0330

    kanban

--- src/pages/Kanban.tsx
+++ src/pages/Kanban.tsx
@@ -1,16 +1,6 @@
-// import { useState } from 'react';
 import TasksContainer from '../components/kanban/TasksContainer';
-// import AddTaskForm from '../components/kanban/AddTaskForm';
-// import Modal from '../components/Modal';
-// import Task from '../entities/Task';
 
 const Kanban = () => {
-	// const [tasks, setTasks] = useState<Task[]>([]);
-
-	// function addTask({ id, title, img, description, status }: Task) {
-	// 	setTasks([{ id, title, img, description, status }, ...tasks]);
-	// }
-
 	return (
 		<div className="kanban-page grid grid-cols-3 gap-x-[1%]">
 			<TasksContainer
