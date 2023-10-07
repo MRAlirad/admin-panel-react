34c3bc3e25c42f53f07b0d12702433397805d7c5
MRAlirad <m.r.alirad75@gmail.com>
2023-10-07 17:46:19 +0330

    bar char container

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -29,12 +29,13 @@ const Dashboard = () => {
 			},
 		],
 	};
+
 	return (
 		<div className="dashboad-page grid grid-cols-12 gap-2.5">
 			<Header />
-			<div className="card col-[1/7] grid grid-rows-[max-content_1fr] gap-5 py-6 h-60">
+			<div className="card col-[1/7] grid grid-rows-[max-content_1fr] gap-5 py-6">
 				<div className="title-box flex items-center justify-between px-[5%]">
-					<h3 className="title text-xl text-delftBlue">درآمد هفتگی</h3>
+					<h3 className="title text-xl text-delftBlue font-bold">درآمد هفتگی</h3>
 					<Button
 						type="icon"
 						text="refresh"
@@ -48,6 +49,31 @@ const Dashboard = () => {
 					labels={weeklyIncome.labels}
 					datasets={weeklyIncome.data}
 				/>
+			</div>
+			<div className="card col-[7/13] grid grid-rows-[max-content_1fr] gap-5 py-6">
+				<div className="title-box flex items-start justify-between px-[5%]">
+					<div className='flex flex-col'>
+						<span className='flex items-center gap-1.5 text-xs text-powderBlue bg-ghostWhite w-max py-1 px-4 rounded-md mb-1'>
+							<i className={`material-icons material-icons-round`}>calendar_today</i>
+							ماه جاری
+						</span>
+						<h3 className="title text-xl text-delftBlue font-bold">37 میلیون تومان</h3>
+						<span className='flex items-center gap-1 text-xs text-powderBlue'>
+							جمع مخارج
+							<span className='flex items-center gap-0.5 text-jade'>
+								<i className="material-icons material-icons-round">arrow_drop_up</i>
+								+2.4%
+							</span>
+						</span>
+					</div>
+					<Button
+						type="icon"
+						text="refresh"
+						size="text-xl"
+						color="blue"
+						className="!bg-ghostWhite"
+					/>
+				</div>
 			</div>
 		</div>
 	);
