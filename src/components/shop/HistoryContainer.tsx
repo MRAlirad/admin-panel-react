9cc8833ebd432cf0370152f50ba30d7c09c2c724
MRAlirad <m.r.alirad75@gmail.com>
2023-10-09 18:22:16 +0330

    rearrange useHistory

--- src/components/shop/HistoryContainer.tsx
+++ src/components/shop/HistoryContainer.tsx
@@ -1,23 +1,13 @@
-import { useQuery } from '@tanstack/react-query';
 // import useHistory from '../../hooks/useHistory';
+import useHistory from '../../hooks/useHistory';
 import Button from '../Button';
 import Loader from '../Loader';
-import History from '../../entities/History';
 import HistoryItem from './HistoryItem';
-import axios from 'axios';
 
 const HistoryContainer = () => {
 	// const { histories, error, isLoading, refresh } = useHistory();
-	const FetchHistory = async ()=> {
-		return axios
-			.get<History[]>("http://localhost:3500/history")
-			.then(res => res.data)
-		;
-	}
-	const {data : histories, error, isLoading} = useQuery<History[], Error>({
-		queryKey: ['history'],
-		queryFn : FetchHistory
-	})
+
+	const {data : histories, error, isLoading} = useHistory()
 
 	return (
 		<section className="history-section card flex flex-col gap-4 h-max p-4">
--- src/hooks/useHistory.ts
+++ src/hooks/useHistory.ts
@@ -1,40 +1,60 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 import History from '../entities/History';
-import historyService from '../services/history-service';
-import { CanceledError } from '../services/api-client';
+// import historyService from '../services/history-service';
+// import { CanceledError } from '../services/api-client';
+
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+// const useHistory = () => {
+// 	const [histories, setHistories] = useState<History[]>([]);
+// 	const [error, setError] = useState('');
+// 	const [isLoading, setIsLoading] = useState(false);
+
+// 	const getHistories = () => {
+// 		setIsLoading(true);
+// 		setError('');
+
+// 		const { request, cancel } = historyService.get<History>();
+// 		request
+// 			.then(response => {
+// 				setHistories(response.data);
+// 				setIsLoading(false);
+// 			})
+// 			.catch(error => {
+// 				if (error instanceof CanceledError) return;
+// 				setError(error.message);
+// 				setIsLoading(false);
+// 			});
+// 		return () => cancel();
+// 	};
+
+// 	const refresh = () => {
+// 		getHistories();
+// 	};
+
+// 	useEffect(() => {
+// 		getHistories();
+// 	}, []);
+
+// 	return { histories, error, isLoading, setHistories, setError, refresh };
+// };
+
+// export default useHistory;
+
 
 const useHistory = () => {
-	const [histories, setHistories] = useState<History[]>([]);
-	const [error, setError] = useState('');
-	const [isLoading, setIsLoading] = useState(false);
-
-	const getHistories = () => {
-		setIsLoading(true);
-		setError('');
-
-		const { request, cancel } = historyService.get<History>();
-		request
-			.then(response => {
-				setHistories(response.data);
-				setIsLoading(false);
-			})
-			.catch(error => {
-				if (error instanceof CanceledError) return;
-				setError(error.message);
-				setIsLoading(false);
-			});
-		return () => cancel();
+	const FetchHistory = async ()=> {
+		return axios
+			.get<History[]>("http://localhost:3500/history")
+			.then(res => res.data)
+		;
 	};
 
-	const refresh = () => {
-		getHistories();
-	};
+	return useQuery<History[], Error>({
+		queryKey: ['history'],
+		queryFn : FetchHistory
+	})
+}
 
-	useEffect(() => {
-		getHistories();
-	}, []);
-
-	return { histories, error, isLoading, setHistories, setError, refresh };
-};
-
-export default useHistory;
+export default useHistory
