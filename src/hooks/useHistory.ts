be89e6f2eedf71d75c56e847b0e55c945a87a415
MRAlirad <m.r.alirad75@gmail.com>
2023-10-09 18:30:18 +0330

    fetch history with apiClient

--- src/hooks/useHistory.ts
+++ src/hooks/useHistory.ts
@@ -1,52 +1,12 @@
-// import { useState, useEffect } from 'react';
 import History from '../entities/History';
-// import historyService from '../services/history-service';
-// import { CanceledError } from '../services/api-client';
-
+import apiClient from '../services/api-client';
 import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
-
-// const useHistory = () => {
-// 	const [histories, setHistories] = useState<History[]>([]);
-// 	const [error, setError] = useState('');
-// 	const [isLoading, setIsLoading] = useState(false);
-
-// 	const getHistories = () => {
-// 		setIsLoading(true);
-// 		setError('');
-
-// 		const { request, cancel } = historyService.get<History>();
-// 		request
-// 			.then(response => {
-// 				setHistories(response.data);
-// 				setIsLoading(false);
-// 			})
-// 			.catch(error => {
-// 				if (error instanceof CanceledError) return;
-// 				setError(error.message);
-// 				setIsLoading(false);
-// 			});
-// 		return () => cancel();
-// 	};
-
-// 	const refresh = () => {
-// 		getHistories();
-// 	};
-
-// 	useEffect(() => {
-// 		getHistories();
-// 	}, []);
-
-// 	return { histories, error, isLoading, setHistories, setError, refresh };
-// };
-
-// export default useHistory;
-
+// import axios from "axios";
 
 const useHistory = () => {
 	const FetchHistory = async ()=> {
-		return axios
-			.get<History[]>("http://localhost:3500/history")
+		return apiClient
+			.get<History[]>("/history")
 			.then(res => res.data)
 		;
 	};
