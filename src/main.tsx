ce15c2043995cc345173e0769141fab7534e565b
MRAlirad <m.r.alirad75@gmail.com>
2023-10-09 18:42:37 +0330

    add devtools

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import router from './routes.tsx';
 import './index.css';
 
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<QueryClientProvider client={queryClient}>
 			<RouterProvider router={router} />
+			<ReactQueryDevtools />
 		</QueryClientProvider>
 	</React.StrictMode>
 );
