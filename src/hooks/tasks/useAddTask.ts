029f458ac38a5bf6683f5e55e579b36b9c2c6991
MRAlirad <m.r.alirad75@gmail.com>
2023-10-11 01:10:59 +0330

    add task

--- src/hooks/tasks/useAddTask.ts
+++ src/hooks/tasks/useAddTask.ts
@@ -1,6 +1,6 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import Task from '../../entities/Task';
-import apiClient from '../../services/api-client';
+import taskService from '../../services/taskService';
 
 interface Props {
 	onAdd?: () => void;
@@ -11,9 +11,7 @@ const useAddtask = ({ onAdd = () => {}, onError = () => {} }: Props) => {
 	const queryClient = useQueryClient();
 
 	return useMutation<Task, Error, Task>({
-		mutationFn: async (task: Task) => {
-			return apiClient.post<Task>('/tasks', task).then(res => res.data);
-		},
+		mutationFn: async (task: Task) => taskService.post(task),
 		onSuccess: addedTask => {
 			queryClient.invalidateQueries({
 				queryKey: ['tasks', { status: addedTask.status }],
