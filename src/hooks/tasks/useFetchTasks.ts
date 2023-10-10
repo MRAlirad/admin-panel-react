91d56b8532632a168e1a7c3b99259212cd10967e
MRAlirad <m.r.alirad75@gmail.com>
2023-10-11 01:11:29 +0330

    fetch task

--- src/hooks/tasks/useFetchTasks.ts
+++ src/hooks/tasks/useFetchTasks.ts
@@ -1,49 +1,12 @@
 import { useQuery } from '@tanstack/react-query';
 import Task from '../../entities/Task';
-import apiClient from '../../services/api-client';
+import taskService from '../../services/taskService';
 
 const useFetchTasks = (status: number) => {
-	const FetchTasks = async () => {
-		return apiClient.get<Task[]>(`/tasks?status=${status}`).then(res => res.data);
-	};
-
 	return useQuery<Task[], Error>({
 		queryKey: ['tasks', { status }],
-		queryFn: FetchTasks,
+		queryFn: () => taskService.get('status', status),
 	});
 };
 
 export default useFetchTasks;
-
-// import { useState, useEffect } from 'react';
-// import Task from '../entities/Task';
-// import tasksService from '../services/tasks-service';
-// import { CanceledError } from '../services/api-client';
-
-// const useTasks = (status: number) => {
-// 	const [tasks, setTasks] = useState<Task[]>([]);
-// 	const [error, setError] = useState('');
-// 	const [isLoading, setIsLoading] = useState(false);
-
-// 	useEffect(() => {
-// 		setIsLoading(true);
-// 		setError('');
-
-// 		const { request, cancel } = tasksService.get<Task>('status', status);
-// 		request
-// 			.then(response => {
-// 				setTasks(response.data);
-// 				setIsLoading(false);
-// 			})
-// 			.catch(error => {
-// 				if (error instanceof CanceledError) return;
-// 				setError(error.message);
-// 				setIsLoading(false);
-// 			});
-// 		return () => cancel();
-// 	}, [status]);
-
-// 	return { tasks, error, isLoading, status, setTasks, setError };
-// };
-
-// export default useTasks;
