2d0cf88f85f49deea9434c0f155312ccacfa2f24
MRAlirad <m.r.alirad75@gmail.com>
2023-10-11 01:11:37 +0330

    api client

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+interface Entity {
+	id: number;
+}
+
 const axiosInstance = axios.create({
 	baseURL: 'http://localhost:3500',
 });
@@ -11,12 +15,22 @@ class APIClient<T> {
 		this.endpoint = endpoint;
 	}
 
-	getAll = async () => {
-		return axiosInstance.get<T[]>(this.endpoint).then(res => res.data);
+	get = async (filterProperty: string = '', filterEntity: string | number = '') => {
+		return axiosInstance
+			.get<T[]>(`${this.endpoint}${filterProperty ? `?${filterProperty}=${filterEntity}` : ''}`)
+			.then(res => res.data);
 	};
 
 	post = async (data: T) => {
 		return axiosInstance.post<T>(this.endpoint, data).then(res => res.data);
+	};
+
+	update<T extends Entity>(entity: T) {
+		return axiosInstance.patch(`${this.endpoint}/${entity.id}`, entity).then(res => res.data);
+	}
+
+	delete = async (id: number) => {
+		return axiosInstance.delete<T>(`${this.endpoint}/${id}`);
 	};
 }
 
