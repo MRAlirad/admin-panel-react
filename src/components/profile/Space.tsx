9d27dfc46113a4a1071c4f63d880099a0ed49245
MRAlirad <m.r.alirad75@gmail.com>
2023-10-13 00:18:47 +0330

    space

---
+++ src/components/profile/Space.tsx
@@ -0,0 +1,21 @@
+import Button from '../Button';
+
+const Space = () => {
+	return (
+		<div className="space-section card relative flex flex-col items-center p-4 pb-6">
+			<Button
+				type="icon"
+				text="more_horiz"
+				className="bg-ghostWhite mr-auto"
+			/>
+			<span className="icon-box flex items-center justify-center h-20 w-20 text-4xl text-delftBlue bg-ghostWhite rounded-full">
+				<i className="material-icons material-icons-outlined">cloud_done</i>
+			</span>
+			<h3 className="text-delftBlue text-xl font-bold mt-3 mb-1">فضای شما</h3>
+			<p className="text-powderBlue text-sm text-center">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت</p>
+			<div className="progress-box mt-auto w-full"></div>
+		</div>
+	);
+};
+
+export default Space;
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,9 +1,11 @@
 import ProfileInfo from '../components/profile/ProfileInfo'
+import Space from '../components/profile/Space';
 const Profile = () => {
 	return(
 		<div className="profile-page">
 			<div className="profile-space-upload-segment grid grid-cols-[38%_24%_34%] gap-[2%] w-full">
 				<ProfileInfo />
+				<Space />
 			</div>
 		</div>
 	);
