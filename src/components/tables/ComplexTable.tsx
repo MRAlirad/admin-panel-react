a677653da5e50c27a479bacb8903cb8582ac82c7
MRAlirad <m.r.alirad75@gmail.com>
2023-10-29 16:35:33 +0330

    complex table

---
+++ src/components/tables/ComplexTable.tsx
@@ -0,0 +1,112 @@
+// import Table from "../Table"
+
+import Button from '../Button';
+import ProgressBar from '../ProgressBar';
+import Status from '../Status';
+
+interface Header {
+	name: string;
+	label: string;
+	sortable?: boolean;
+}
+
+interface Complex {
+	name: string;
+	status: number;
+	data: string;
+	progresss: number;
+}
+
+const ComplexTable = () => {
+	const headers: Header[] = [
+		{
+			name: 'name',
+			label: 'نام',
+		},
+		{
+			name: 'status',
+			label: 'وضعیت',
+		},
+		{
+			name: 'history',
+			label: 'تاریخ',
+		},
+		{
+			name: 'progress',
+			label: 'پیشرفت',
+		},
+	];
+	const rows: Complex[] = [
+		{
+			name: 'لورم ایپسوم',
+			status: 0,
+			data: '24.فرو.1402',
+			progresss: 60,
+		},
+		{
+			name: 'لورم ایپسوم',
+			status: 1,
+			data: '24.فرو.1402',
+			progresss: 40,
+		},
+		{
+			name: 'لورم ایپسوم',
+			status: 2,
+			data: '24.فرو.1402',
+			progresss: 80,
+		},
+	];
+
+	return (
+		<div className="table-container card w-full p-5">
+			<div className="title-box flex items-center justify-between mb-5">
+				<h2 className="title">جدول کمپلکس</h2>
+				<Button
+					type="icon"
+					text="more_horiz"
+					className="bg-ghostWhite"
+				/>
+			</div>
+			<table className="w-full">
+				<thead>
+					<tr className="header">
+						{headers.map((header) => (
+							<td key={header.name} className=' border-b border-solid border-[#E9EDF7] pb-3'>
+								<div
+									className={`flex items-center justify-center gap-1 text-sm text-powderBlue ${
+										header.sortable ? 'cursor-pointer' : 'cursor-default'
+									}`}
+								>
+									<span>{header.label}</span>
+									{header.sortable ? (
+										<i className="material-icons material-icons-round"> keyboard_arrow_down</i>
+									) : null}
+								</div>
+							</td>
+						))}
+					</tr>
+				</thead>
+				<tbody>
+					{rows.map((row) => (
+						<tr key={row.name} className='text-center h-11'>
+							<td>
+								<span className='text-sm font-bold text-delftBlue'>{row.name}</span>
+							</td>
+							<td>
+								<Status state={row.status} />
+							</td>
+							<td>
+								<span className='text-sm font-bold text-delftBlue'>{row.data}</span>
+							</td>
+							<td>
+								<ProgressBar percent={row.progresss} />
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
+};
+
+export default ComplexTable;
