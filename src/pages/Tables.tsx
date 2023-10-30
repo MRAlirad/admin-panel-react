55a27dcf3fa6fe1fa491c9cea91f2ab0d4ecbc09
MRAlirad <m.r.alirad75@gmail.com>
2023-10-30 12:45:46 +0330

    table page

--- src/pages/Tables.tsx
+++ src/pages/Tables.tsx
@@ -1,9 +1,11 @@
 import ComplexTable from "../components/tables/ComplexTable";
+import DevelopmentTable from "../components/tables/DevelopmentTable";
 
 const Tables = () => {
 	return(
 		<div className="grid grid-cols-2 gap-3">
 			<ComplexTable />
+			<DevelopmentTable />
 		</div>
 	);
 };
