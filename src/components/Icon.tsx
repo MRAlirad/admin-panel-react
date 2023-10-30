fcf91911e95a1495bbda9ea76857ee8451fa5a57
MRAlirad <m.r.alirad75@gmail.com>
2023-10-30 13:39:14 +0330

    icon

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -7,7 +7,7 @@ interface Props {
 const Icon = ({ icon, color = 'text-powderBlue', size = 'text-sm' }: Props) => {
 	return (
 		<span className={`flex items-center justify-center ${color} ${size}`}>
-			<i className="material-icons material-icons-round">{icon}</i>
+			<i className={`material-icons material-icons-${icon.split(' ')[1] ?? 'round'}`}>{icon.split(' ')[0]}</i>
 		</span>
 	);
 };
