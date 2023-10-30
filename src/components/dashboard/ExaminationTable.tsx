2790ec30d4085264a8424146b3d8fc6748354612
MRAlirad <m.r.alirad75@gmail.com>
2023-10-30 12:45:26 +0330

    examination table

--- src/components/dashboard/ExaminationTable.tsx
+++ src/components/dashboard/ExaminationTable.tsx
@@ -1,23 +1,23 @@
-import Table from '../Table';
+// import Table from '../Table';
 
 const ExaminationTable = () => {
-	const headersList = [
-		{ name: 'name', label: 'نام' },
-		{ name: 'status', label: 'وضعیت' },
-		{ name: 'date', label: 'تاریخ' },
-		{ name: 'progress', label: 'پیشرفت' },
-	];
-	const record = [
-		{
-			name: 'محمدرضا',
-			status: 0,
-			date: '5 فروردین',
-			progress: 50,
-		}
-	]
+	// const headersList = [
+	// 	{ name: 'name', label: 'نام' },
+	// 	{ name: 'status', label: 'وضعیت' },
+	// 	{ name: 'date', label: 'تاریخ' },
+	// 	{ name: 'progress', label: 'پیشرفت' },
+	// ];
+	// const record = [
+	// 	{
+	// 		name: 'محمدرضا',
+	// 		status: 0,
+	// 		date: '5 فروردین',
+	// 		progress: 50,
+	// 	}
+	// ]
 	return (
 		<div className="card col-[7/13] py-4 px-5">
-			<Table headers={headersList} rows={record} />
+			{/* <Table headers={headersList} rows={record} /> */}
 		</div>
 	);
 };
